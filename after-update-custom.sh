34a326d22a27714ed5e080832e6d42b72d9db222
Williams3993 <162794307+Williams3993@users.noreply.github.com>
2024-12-01 15:55:59 +0800

    Update after-update-custom.sh

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -8,5 +8,5 @@ sed -i 's/192.168.1.1/192.168.69.1/g' package/base-files/files/bin/config_genera
 #sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
 #sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
 # sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/LEAN-GL-AXT1800/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/LEAN-GL-AXT1800/g' package/base-files/files/bin/config_generate
 
