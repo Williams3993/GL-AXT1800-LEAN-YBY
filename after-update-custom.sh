a1f1e70ce6d9aa6a2f0ca3f2ec942672f012d6f2
Williams3993 <162794307+Williams3993@users.noreply.github.com>
2024-12-07 22:26:37 +0800

    Update after-update-custom.sh

--- after-update-custom.sh
+++ after-update-custom.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
-# Description: (After Update feeds)
+# Step 1: 修改默认 IP 地址
+# 将默认的 192.168.1.1 修改为 192.168.89.1
+sed -i 's/192.168.1.1/192.168.89.1/g' package/base-files/files/bin/config_generate
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.69.1/g' package/base-files/files/bin/config_generate
+# Step 2: 修改主机名
+# 将默认的主机名 "OpenWrt" 修改为 "OpenWrt23-X86-X64"
+# 主机名不能是纯数字，也不能使用中文字符
+sed -i 's/OpenWrt/OpenWrt23-X86-X64/g' package/base-files/files/bin/config_generate
 
-# Set DHCP IP Address start and end
-#sed -i "s/option start.*/option start \'2\'/g" package/network/services/dnsmasq/files/dhcp.conf
-#sed -i "s/option limit.*/option limit \'55\'/g" package/network/services/dnsmasq/files/dhcp.conf
-# sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
-sed -i 's/LEDE/LEAN-GL-AXT1800/g' package/base-files/files/bin/config_generate
+# Step 3: 修改默认时区
+# 将默认时区从 "UTC" 修改为 "CST-8"
+sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
 
+# Step 4: 增加时区名称设置为 "Asia/Shanghai"
+# 在时区设置后添加区域名称设置
+sed -i "/set system.\@system\[-1\].timezone='CST-8'/a\ \ \ \ set system.\@system\[-1\].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
